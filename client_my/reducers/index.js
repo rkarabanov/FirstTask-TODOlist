83feb16a37c18c7138328215e1c024dc9f34623d
Raman Karabanau <5600t0@gmail.com>
2017-04-20 18:17:56 +0300

    Visual Chenges Props

--- client_my/reducers/index.js
+++ client_my/reducers/index.js
@@ -8,12 +8,19 @@ const initialState = {
 export default function login(state = initialState, action) {
     switch (action.type) {
         case 'IS_LOGIN':
-            res.login(action.payload).then(function(response){
-                if(response.data.length==0){
-                return {...state, msg:"Ошибка ввода: Неверной email/пароль"};}
-                else{
-                browserHistory.redirect("/dashboard");
-        }});
+        //     res.login(action.payload).then(function(response){
+        //         if(response.data=="Ошибка ввода: Неверной email/пароль"){
+        //             console.log(response.data);
+        //         return {...state, msg:response.data};}
+        //         else{
+        //         browserHistory.push("/dashboard");
+        //             return {...state,user:response.data};
+        // }})
+            let msg=res.login(action.payload).then(function(response){return response.data});
+           
+
+            return {...state, msg:""+msg};
+            ;
         default:
             return state;
     }
