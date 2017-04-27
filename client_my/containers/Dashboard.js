8569e85dd23c50fc371bc6903deab42901e81a42
Raman Karabanau <5600t0@gmail.com>
2017-04-27 11:19:58 +0300

    Add 3 tasks without input-field validation

--- client_my/containers/Dashboard.js
+++ client_my/containers/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 export default class Dashboard extends Component {
     render() {
         const {  userInSystem } = this.props;
@@ -7,6 +8,7 @@ export default class Dashboard extends Component {
             <div>
                 Your email is {userInSystem.email}
             </div>
+            <Link  to="/admin">Администратор?</Link>
         </div>
     }
 
