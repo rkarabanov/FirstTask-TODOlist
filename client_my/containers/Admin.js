51142b689ef61ba2009bc120971f4bd79e344e0d
Raman Karabanau <5600t0@gmail.com>
2017-04-29 09:39:45 +0300

    /admin add matirial ui

--- client_my/containers/Admin.js
+++ client_my/containers/Admin.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import * as loadCompAction from '../actions/LoadComponentAction'
+import * as login from '../actions/LoginAction'
+import LoadingPage from "./LoadingPage"
+import enums from "../constans/Const"
+import { Paper} from 'material-ui'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import '../css/main.css'
 
-class Info extends Component {
+class Admin extends Component {
+
+    componentWillMount() {
+        this.props.isInSystem();
+    }
+
+
     render() {
         console.log(this.props.userInSystem);
-        if (this.props.userInSystem.role!=undefined&&this.props.userInSystem.role=='admin'){
-            return  <div><h2>Welcome,</h2>
-            {this.props.userInSystem.email}</div>}
-            else{
-            return  <div>HTTP Forbidden</div>
+        switch (this.props.loadingStatus) {
+            case enums.LOAD_REQUEST:
+                return <LoadingPage/>;
+                break;
+            default: {
+                if (this.props.userInSystem.role!=undefined&&this.props.userInSystem.role=='admin') return(
+                    <MuiThemeProvider>
+                        <Paper>
+                            <div className="main-container">
+                                <div>
+                                    <h3>Welcome, {this.props.userInSystem.email}, Вы в Admin!</h3>
+                                </div>
+                            </div>
+                        </Paper>
+                    </MuiThemeProvider>);
+                else  return(
+                    <MuiThemeProvider>
+                        <Paper>
+                            <div className="main-container">
+                                <div>
+                                    <h3>HTTP Forbidden 403</h3>
+                                </div>
+                            </div>
+                        </Paper>
+                    </MuiThemeProvider>
+                )
+            }
         }
-        }
+    }
+    componentWillUnmount() {
+        this.props.loadComponentAction();
+    }
 }
 
 
 
 function mapStateToProps (state) {
-    console.log(state);
     return {
-        userInSystem:state.userInSystem
+        userInSystem:state.userInSystem,
+        loadingStatus:state.loadingStatus
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        isInSystem:bindActionCreators(login.isInSystem, dispatch),
+        loadComponentAction:bindActionCreators(loadCompAction.loadComponentAction, dispatch)
     }
 }
 
 
-export default connect(mapStateToProps)(Info)
+export default connect(mapStateToProps,mapDispatchToProps)(Admin)
